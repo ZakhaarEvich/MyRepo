7f90d0f46fbbbbf3a851084771f748b55661cbff
ZakhaarEvich <zakharzakharenko@outlook.com>
2023-03-21 12:22:11 +0300

    Check

--- lab4/lab4/src/main/java/Main.java
+++ lab4/lab4/src/main/java/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Figure circle = new Circle(5, 0, 0);
-        Figure rectangle = new Rectangle(3, 4);
-        Figure triangle = new Triangle(3, 4, 5);
-        Figure ellipse = new Ellipse(5, 3);
-
+        // Создаем объекты фигур: круг, прямоугольник, треугольник, эллипс
+        Figure circle = new Circle(5, 0, 0); // круг с радиусом 5 и центром в точке (0,0)
+        Figure rectangle = new Rectangle(3, 4); //прямоугольник со сторонами 3 и 4
+        Figure triangle = new Triangle(3, 4, 5); // треугольник со сторонами 3, 4 и 5
+        Figure ellipse = new Ellipse(5, 3); //эллипс с большой полуосью 5 и малой полуосью 3
+        // Выводим на экран площадь и периметр каждой фигуры
         System.out.println("Circle area: " + circle.getArea());
         System.out.println("Circle perimeter: " + circle.getPerimeter());
 
--- lab4/lab4/src/main/java/Triangle.java
+++ lab4/lab4/src/main/java/Triangle.java
@@ -1,20 +1,22 @@
+// Объявляем класс Triangle, который наследует класс Figure
 public class Triangle extends Figure {
+    // Объявляем приватные переменные sideA, sideB и sideC типа double
     private double sideA;
     private double sideB;
     private double sideC;
-
+    // Объявляем конструктор класса Triangle с параметрами sideA, sideB и sideC
     public Triangle(double sideA, double sideB, double sideC) {
         this.sideA = sideA;
         this.sideB = sideB;
         this.sideC = sideC;
     }
-
+    // Переопределяем метод getArea() из класса Figure
     @Override
     public double getArea() {
-        double p = (sideA + sideB + sideC) / 2; // полупериметр
-        return Math.sqrt(p * (p - sideA) * (p - sideB) * (p - sideC)); // формула Герона
+        double p = (sideA + sideB + sideC) / 2; //  вычисление полупериметра
+        return Math.sqrt(p * (p - sideA) * (p - sideB) * (p - sideC)); // вычисление площади по формуле Герона
     }
-
+    // Переопределяем метод getPerimeter() из класса Figure
     @Override
     public double getPerimeter() {
         return sideA + sideB + sideC;
