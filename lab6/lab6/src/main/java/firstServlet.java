39517ccbc63c6a5b83ab22b8521471d98957ca16
ZakhaarEvich <zakharzakharenko@outlook.com>
2023-04-16 20:11:57 +0300

    @author Zakhar Zakharenko

--- lab6/lab6/src/main/java/firstServlet.java
+++ lab6/lab6/src/main/java/firstServlet.java
@@ -2,18 +2,32 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-
-import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.IOException;
 
-public class firstServlet extends HttpServlet {
+public class firstServlet extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+// Получаем параметры запроса
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+
+// Создаем экземпляр класса, который будет обрабатывать параметры запроса
+        FormProcessor formProcessor = new FormProcessor();
+
+// Обрабатываем параметры запроса и получаем результат
+        String result = formProcessor.processForm(name, email);
+
+// Отправляем ответ клиенту
+        PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h2>This is lab6 firstServlet Class<h2>");
+        out.println("<h1>Form Submit Result:</h1>");
+        out.print(result);
         out.println("</body></html>");
     }
 }
--- lab6/lab6/src/main/webapp/index.jsp
+++ lab6/lab6/src/main/webapp/index.jsp
@@ -1,5 +1,17 @@
+<!DOCTYPE html>
 <html>
+<head>
+    <title>Example Form</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<form method="POST" action="/hello">
+    <label for="name">Name:</label>
+    <input type="text" name="name" id="name">
+    <br>
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email">
+    <br>
+    <input type="submit" value="Submit">
+</form>
 </body>
 </html>
